b0a4437d2a4a0029251eba60cafec3315059bf76
hdavidpb <hernan.plazabs@gmail.com>
2021-03-09 21:34:49 -0500

    version3

--- src/assets/img/imagesProyects.js
+++ src/assets/img/imagesProyects.js
@@ -4,42 +4,49 @@ import proyect3 from "./img3.jpg";
 import proyect4 from "./img4.jpg";
 import proyect5 from "./img5.jpg";
 import proyect6 from "./img6.png";
+import proyect7 from "./img7.png";
 
 export default [
-	{
-		proyect: proyect6,
-		titlle: "TODO LIST EN REACT.JS",
-		href: "https://rememberreact.netlify.app/",
-		key: 1,
-	},
-	{
-		proyect: proyect1,
-		titlle: "API PAISES",
-		href: "https://api-paises.netlify.app/",
-		key: 2,
-	},
-	{
-		proyect: proyect3,
-		titlle: "JUEGO DE TRIVIA",
-		href: "https://triviah.netlify.app/",
-		key: 3,
-	},
-	{
-		proyect: proyect4,
-		titlle: "CRUD SIMPLE EN VUE",
-		href: "https://pendientes-tsi.netlify.app/",
-		key: 4,
-	},
-	{
-		proyect: proyect5,
-		titlle: "SOCIAL PROOF",
-		href: "https://socialproof-challenge.netlify.app/",
-		key: 5,
-	},
-	{
-		proyect: proyect2,
-		titlle: "PORTAFOLIO PSICOLOGIA",
-		href: "https://psicologakathebarrios.netlify.app/",
-		key: 6,
-	},
+  {
+    proyect: proyect7,
+    titlle: "NOTES",
+    href: "https://agregarnotas-5a95d.web.app/login",
+    key: 7,
+  },
+  {
+    proyect: proyect6,
+    titlle: "TODO LIST EN REACT.JS",
+    href: "https://rememberreact.netlify.app/",
+    key: 1,
+  },
+  {
+    proyect: proyect3,
+    titlle: "JUEGO DE TRIVIA",
+    href: "https://triviah.netlify.app/",
+    key: 3,
+  },
+  {
+    proyect: proyect4,
+    titlle: "CRUD SIMPLE EN VUE",
+    href: "https://pendientes-tsi.netlify.app/",
+    key: 4,
+  },
+  {
+    proyect: proyect5,
+    titlle: "SOCIAL PROOF",
+    href: "https://socialproof-challenge.netlify.app/",
+    key: 5,
+  },
+  {
+    proyect: proyect2,
+    titlle: "PORTAFOLIO PSICOLOGIA",
+    href: "https://psicologakathebarrios.netlify.app/",
+    key: 6,
+  },
+  {
+    proyect: proyect1,
+    titlle: "API PAISES",
+    href: "https://api-paises.netlify.app/",
+    key: 2,
+  },
 ];
